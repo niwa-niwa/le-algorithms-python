24a4f5a80023dd4a9e63b613b9f5d3ec0477ac03
niwa-niwa <urban.hang274@gmail.com>
2021-06-09 00:30:55 +0900

    completed merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -3,7 +3,7 @@ nums = [5, 4, 1, 8, 7, 3, 2, 9]
 
 def merge_sort(numbers):
     if len(numbers) == 1:
-        return numbers
+        return 
 
     center = len(numbers) // 2
     left = numbers[:center]
@@ -12,9 +12,30 @@ def merge_sort(numbers):
     merge_sort(left)
     merge_sort(right)
 
-    i = j = k = 0
+    left_index = right_index = merge_index = 0
+
+    while left_index <= len(left)-1 and right_index <= len(right)-1:
+        if left[left_index] < right[right_index]:
+            numbers[merge_index] = left[left_index]
+            left_index += 1
+        else:
+            numbers[merge_index] = right[right_index]
+
+            right_index += 1
+
+        merge_index += 1
+
+    while right_index < len(right):
+        numbers[merge_index] = right[right_index]
+        right_index += 1
+        merge_index += 1
+
+    while left_index < len(left):
+        numbers[merge_index] = left[left_index]
+        left_index += 1
+        merge_index += 1
+
+    return numbers
 
 
-    print(numbers)
-
-merge_sort(nums)
+print(merge_sort(nums))
