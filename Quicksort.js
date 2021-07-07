4fc8e4bc870f531b4fc95d5a6edfa13beb5cfd60
niwa-niwa <urban.hang274@gmail.com>
2021-07-08 08:00:13 +0900

    fixed infinity roop

--- Quicksort.js
+++ Quicksort.js
@@ -2,9 +2,12 @@ console.log('test script')
 
 function quickSort(numbers){
 
-  const _partition = (numbers, low, high) => {
-    i = low - 1;
-    for(j = 0; j < numbers.length; j++){
+  const _partition = (nums, low, high) => {
+    
+    const numbers = [...nums]
+    let i = low - 1;
+    for(let j = low; j < numbers.length; j++){
+      
       if(numbers[j] < numbers[high] ){
         i++;
         [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
@@ -12,16 +15,15 @@ function quickSort(numbers){
     }
     i++;
     [numbers[i], numbers[high]] = [numbers[high], numbers[i]];
-    return i
+    return {index:i, numbers:numbers}
   }
 
   const _quickSort = (numbers, low, high) => {
     if( low < high ){
       let partition = _partition(numbers, low, high);
-      _quickSort(numbers, low, partition-1);
-      _quickSort(numbers, partition+1, high);
+      _quickSort(partition.numbers, low, partition.index-1);
+      _quickSort(partition.numbers, partition.index+1, high);
     }
-    console.log(numbers);
   }
 
   _quickSort(numbers, 0, numbers.length-1);
@@ -29,5 +31,14 @@ function quickSort(numbers){
 
 }
 
-let nums = [1,8,5,9,4,3,7];
-console.log(quickSort(nums));
+const nums = [1,8,5,9,4,3,7];
+const result = quickSort(nums);
+console.log("result = ",result);
+// console.log(quickSort(nums));
+// console.log(nums)
+// [nums[0], nums[5]] = [nums[5], nums[0]]
+// console.log(nums)
+
+// const arr = [1,2,3];
+// [arr[2], arr[1]] = [arr[1], arr[2]];
+// console.log(arr); // [1,3,2]
